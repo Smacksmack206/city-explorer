f8a25a02fdea7d0514c560b135f8bfc00266677c
Cedric Vallieu <cvallieu94@gmail.com>
2022-01-03 19:34:14 -0800

    completed map feature in TO:DO list in Trello

--- src/App.js
+++ src/App.js
@@ -31,9 +31,11 @@ class App extends React.Component {
       <input onChange={this.saveCitySearchedFor} placeholder='search for a city'>
       </input>
       <button onClick={this.getLocation}>Explore!</button>
+      
       {this.state.locationObject.place_id &&
       <h2>The city we searched for is: {this.state.locationObject.display_name}</h2>
      }
+     <h3>{this.state.locationObject.lat}  {this.state.locationObject.lon}</h3>
      <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${this.state.locationObject.lat},${this.state.locationObject.lon}&zoom=12`} alt={this.state.locationObject.display_name}/>
       </div>
     );
